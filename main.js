e3d146258a4ccb11b70fdf55aaca219184a3c7e7
Ahmadshah35 <ahmadshah102005@gmail.com>
2024-04-11 13:45:27 -0700

    created a currency converter

---
+++ main.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //base currency
+    INR: 74.57,
+    PKR: 280,
+    EUR: 0.91
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "enter from currency",
+        type: "list",
+        choices: ["USD", "INR", "PKR", "INR", "EUR"]
+    },
+    {
+        name: "to",
+        message: "enter to currency",
+        type: "list",
+        choices: ["USD", "INR", "PKR", "INR", "EUR"]
+    },
+    {
+        name: "amount",
+        message: "enter your amount",
+        type: "number",
+    },
+]);
+let fromamount = currency[userAnswer.from];
+let toamount = currency[userAnswer.to];
+let amount = userAnswer.amount;
+let baseamount = amount / fromamount;
+let convertedamount = baseamount * toamount;
+let afterDecimal = 2;
+console.log(convertedamount);
